d157cd1e0df0b9116e272f02cef8e2b1e85d8c1d
Vinayak Banga <vinayakbanga22@gmail.com>
2022-12-29 16:27:13 +0530

    Timeline complete

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import data from "../assests/data.json"
 
 const timeline = () => {
   return (
-    <div id='timeline'>timeline</div>
+    <div id='timeline'>
+        <div className="timelineBox">
+            {
+                data.projects.map((item,index)=>(
+                    <TimelineItem heading={item.title} text={item.date} index={index} key ={item.title}/>
+                ))
+            }
+
+
+        </div>
+    </div>
   )
 }
 
+
+const TimelineItem =({heading,text,index})=>(
+    <div className={`timelineItem 
+    ${index%2===0 ? "leftTimeline":"rightTimeline"}`
+    }>
+
+        <div>
+        <h2>{heading}</h2>
+        <p>{text}</p>
+        </div>
+        
+    </div>
+)
 export default timeline
