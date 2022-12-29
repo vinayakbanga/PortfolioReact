9e3026265187feee10f9b785c136dafb69abdaf0
vinayakbanga <72224428+vinayakbanga@users.noreply.github.com>
2022-12-29 11:04:59 +0000

    Starting with services section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Home from "./components/Home.jsx";
 import Work from "./components/Work.jsx";
 import Timeline from "./components/timeline.jsx";
+import Services from "./components/Services.jsx";
 
 function App() {
   return (
